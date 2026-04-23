0f7aa2fdb4a40a7457d03fa97d93030ff907628d
ofts-cqm <qianmuchen.sam@gmail.com>
2026-04-23 19:30:39 -0400

    refactor: remove unused import

--- src/client/java/net/ofts/artist/client/Commands.java
+++ src/client/java/net/ofts/artist/client/Commands.java
@@ -2,7 +2,6 @@ package net.ofts.artist.client;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
